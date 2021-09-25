6c08a24d67f6dc099cff9d0e9b3b5386c694a42c
Joshua <00159817@uca.edu.sv>
2021-09-24 19:33:00 -0600

    Repositories

--- app/src/main/java/com/proceedto15/wb/database/repositories/OrdenRepository.kt
+++ app/src/main/java/com/proceedto15/wb/database/repositories/OrdenRepository.kt
@@ -1,7 +1,9 @@
 package com.proceedto15.wb.database.repositories
 
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
 import com.proceedto15.wb.database.daos.*
+import com.proceedto15.wb.database.entities.*
 
 class OrdenRepository (
     private val PagoDAO: PagoDAO,
@@ -12,5 +14,101 @@ class OrdenRepository (
     private val MarcaDAO: MarcaDAO,
     private val HistorialVentaDAO: HistorialVentaDAO) {
 
+    val allPago: LiveData<List<Pago>> = PagoDAO.getAllPago()
+    val allOrden: LiveData<List<Orden>> = OrdenDAO.getAllOrden()
+    val allOrdenDetalle: LiveData<List<OrdenDetalle>> = OrdenDetalleDAO.getAllOrdenDetalle()
+    val allProducto: LiveData<List<Producto>> = ProductoDAO.getAllProducto()
+    val allCategoriaProducto: LiveData<List<CategoriaProducto>> = CategoriaProductoDAO.getAllCategoriaProducto()
+    val allMarca: LiveData<List<Marca>> = MarcaDAO.getAllMarca()
+    val allHistorialVenta: LiveData<List<HistorialVenta>> = HistorialVentaDAO.getAllHistorialVenta()
 
+    //Inserts
+
+    @WorkerThread
+    suspend fun insertPago(pago: Pago){
+        PagoDAO.insert(pago)
+    }
+
+    @WorkerThread
+    suspend fun insertOrden(orden: Orden){
+        OrdenDAO.insert(orden)
+    }
+
+    @WorkerThread
+    suspend fun insertOrdenDetalle(ordenDetalle: OrdenDetalle){
+        OrdenDetalleDAO.insert(ordenDetalle)
+    }
+
+    @WorkerThread
+    suspend fun insertProducto(producto: Producto){
+        ProductoDAO.insert(producto)
+    }
+
+    @WorkerThread
+    suspend fun insertCategoriaProducto(categoriaProducto: CategoriaProducto){
+        CategoriaProductoDAO.insert(categoriaProducto)
+    }
+
+    @WorkerThread
+    suspend fun insertMarca(marca: Marca){
+        MarcaDAO.insert(marca)
+    }
+
+    @WorkerThread
+    suspend fun insertHistorialVenta(historialVenta: HistorialVenta){
+        HistorialVentaDAO.insert(historialVenta)
+    }
+
+    //GET
+
+    fun getPago(id: Int) = PagoDAO.getPago(id)
+
+    fun getOrden(id: Int) = OrdenDAO.getOrden(id)
+
+    fun getOrdenDetalle(id: Int) = OrdenDetalleDAO.getOrdenDetalle(id)
+
+    fun getProducto(id: Int) = ProductoDAO.getProducto(id)
+
+    fun getCategoriaProducto(id: Int) = CategoriaProductoDAO.getCategoriaProducto(id)
+
+    fun getMarca(id: Int) = MarcaDAO.getMarca(id)
+
+    fun getHistorialVenta(id: Int) = HistorialVentaDAO.getHistorialVenta(id)
+
+    //Delete
+
+    @WorkerThread
+    suspend fun nukePago(){
+        return PagoDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeOrden(){
+        return OrdenDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeOrdenDetalle(){
+        return OrdenDetalleDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeProducto(){
+        return ProductoDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeCategoriaProducto(){
+        return CategoriaProductoDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeMarca(){
+        return MarcaDAO.nukeTable()
+    }
+
+    @WorkerThread
+    suspend fun nukeHistorialVenta(){
+        return HistorialVentaDAO.nukeTable()
+    }
 }
