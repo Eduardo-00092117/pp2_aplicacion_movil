e307fb55dca69ba98c5abcca6a3dbb7f0a8c5ef5
MaPache07 <00170517@uca.edu.sv>
2021-11-18 16:35:15 -0600

    Payment: Corrigiendo bugs

--- app/src/main/java/com/proceedto15/wb/ui/activities/CreditCardActivity.kt
+++ app/src/main/java/com/proceedto15/wb/ui/activities/CreditCardActivity.kt
@@ -1,18 +1,14 @@
 package com.proceedto15.wb.ui.activities
 
+import android.app.Activity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.braintreepayments.cardform.view.CardForm
 import com.proceedto15.wb.R
-import com.proceedto15.wb.databinding.ActivityCashBinding
-import com.proceedto15.wb.databinding.ActivityPaymentCreditcardBinding
 
 class CreditCardActivity : AppCompatActivity() {
 
@@ -21,21 +17,26 @@ class CreditCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_creditcard)
-        val i = intent.extras
-        initView(i)
-        }
+        initView(intent.extras)
+    }
 
-        fun initView(i: Bundle?){
-            val s = i?.getString("value")
-            //Log.d("Msg", i.toString())
-            val cardform = findViewById<CardForm>(R.id.cardform)
-            cardform.cardholderName(CardForm.FIELD_REQUIRED)
-                .cardRequired(true)
-                .expirationRequired(true)
-                .cvvRequired(true)
-                .actionLabel("Purchase")
-                .setup(this)
-        }
+    fun initView(i: Bundle?){
+        findViewById<Button>(R.id.pay_button).setOnClickListener(paymentClickListener)
+
+        val cardform = findViewById<CardForm>(R.id.cardform)
+        cardform.cardholderName(CardForm.FIELD_REQUIRED)
+            .cardRequired(true)
+            .expirationRequired(true)
+            .cvvRequired(true)
+            .actionLabel("Purchase")
+            .setup(this)
+    }
+
+    val paymentClickListener = View.OnClickListener {
+        Toast.makeText(this, getString(R.string.payment_product), Toast.LENGTH_LONG).show()
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
 }
 
 
--- app/src/main/java/com/proceedto15/wb/ui/activities/PaymentActivity.kt
+++ app/src/main/java/com/proceedto15/wb/ui/activities/PaymentActivity.kt
@@ -1,32 +1,54 @@
 package com.proceedto15.wb.ui.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.proceedto15.wb.R
+import com.proceedto15.wb.database.viewmodels.OrdenViewModel
+import com.proceedto15.wb.utilities.AppConstants.ACT_FOR_RESULT
+import com.proceedto15.wb.utilities.AppConstants.PAYMENT_KEY
 
 class PaymentActivity : AppCompatActivity() {
+
+    private lateinit var ordenViewModel: OrdenViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paymentpage)
-        val i = intent
-        val s = i.getStringExtra("value")
-        Log.d("Msg", s.toString())
-        actions(s.toString())
+        ordenViewModel = ViewModelProvider(this).get(OrdenViewModel::class.java)
+
+        actions(intent.getStringExtra(PAYMENT_KEY).toString())
     }
 
     private fun actions(i: String){
-        Log.d("Msg", i)
         findViewById<Button>(R.id.efectivo).setOnClickListener{
-
-
+            Toast.makeText(this, getString(R.string.reserved_products), Toast.LENGTH_LONG).show()
+            modifyDB()
+            finish()
         }
         findViewById<Button>(R.id.tarjeta).setOnClickListener {
             val intent = Intent(this, CreditCardActivity::class.java)
-            intent.putExtra("value", i)
-            startActivity(intent)
+            intent.putExtra(PAYMENT_KEY, i)
+            startActivityForResult(intent, ACT_FOR_RESULT)
+        }
+    }
+
+    private fun modifyDB(){
+        /* ordenViewModel.allPedidos.value?.forEach {
+            ordenViewModel.subtractExistence(it.id, it.Qty)
+        } */
+        ordenViewModel.deleteAllPedido()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode.equals(Activity.RESULT_OK)){
+            modifyDB()
+            finish()
         }
     }
 }
