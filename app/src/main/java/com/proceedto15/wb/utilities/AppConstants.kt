99e5fc37c09eadf8be9941e7a10369d25874668a
MaPache07 <00170517@uca.edu.sv>
2021-11-18 16:04:16 -0600

    Utilities: Agregando constantes

--- app/src/main/java/com/proceedto15/wb/utilities/AppConstants.kt
+++ app/src/main/java/com/proceedto15/wb/utilities/AppConstants.kt
@@ -3,4 +3,6 @@ package com.proceedto15.wb.utilities
 object AppConstants {
     val RC_SIGN_IN = 785
     val SELECTED_SERVICES_KEY = "selectedServices"
+    val PAYMENT_KEY = "paymentKey"
+    val ACT_FOR_RESULT = 1
 }
